f2aad12ca673e3b21dd33d4040949a20a0f1b3d4
Felix Menzel <FelixinLondon@googlemail.com>
2020-08-17 22:28:15 +0200

    Added super basic controller

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,32 @@
+class BikesController < ApplicationController
+
+    def new
+        @user = User.find(current_user.id)
+        @bike = Bike.new
+    end
+
+    def create
+        @bike = Bike.new(bike_params)
+        @user = User.find(current_user.id)
+        @bike.user = @user
+        if @bike.save
+            redirect_to @bike, notice: 'Bike was added'
+        else
+            render :new
+        end
+
+    end
+
+    def show
+        @bike = Bike.find(params[:id])
+
+    end
+    
+    private
+
+    def bike_params
+        params.require(:bike).permit(:title)
+        
+    end
+    
+end
