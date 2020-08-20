8d69a4f200f3408bdcf3f0a31067d124a1f9e2c4
Minh Vi Nguyen <minhvi@hotmail.de>
2020-08-20 15:08:26 +0200

    css profile etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:edit_user_registration_path) do |user_params|
+    user_params.permit(:photo, :username, :about)
+    end
+  end
 end
