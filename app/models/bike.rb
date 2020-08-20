7849fc188b1dc6d7fe369fc3655e042f4eb8b9c5
Felix Menzel <FelixinLondon@googlemail.com>
2020-08-20 17:59:55 +0200

    unbreaking MAster

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,6 +1,7 @@
 class Bike < ApplicationRecord
     belongs_to :user, foreign_key: "user_id"
     has_one_attached  :photo
+    belongs_to :district, foreign_key: "district_id"
     include PgSearch::Model
     pg_search_scope :search_by_title_and_description,
       against: [ :title, :description ],
