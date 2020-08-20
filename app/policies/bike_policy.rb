efa8c938f0448ae8ca238be7ac051d5c00eb34be
Felix Menzel <FelixinLondon@googlemail.com>
2020-08-20 13:03:34 +0200

    PunditisWorking

---
+++ app/policies/bike_policy.rb
@@ -0,0 +1,35 @@
+class BikePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    return true
+  end
+
+  def new?
+    return true
+  end
+
+  def edit?
+    update?
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  def show?
+    return true
+  end
+end
