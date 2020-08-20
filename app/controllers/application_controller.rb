9417bf5e5c007e9c49a8568e9542ba8e8b9d921e
Felix Menzel <36296555+FelixMZ2018@users.noreply.github.com>
2020-08-20 15:39:16 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:edit_user_registration_path) do |user_params|
-    user_params.permit(:photo, :username, :about)
-    end
+    devise_parameter_sanitizer.permit(:account_update, keys: [:about,:password,:username,:photo])
   end
 end
