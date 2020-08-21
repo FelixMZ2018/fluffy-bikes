c615af146729fc211083250d65421f1b8d439f92
Felix Menzel <FelixinLondon@googlemail.com>
2020-08-21 10:01:54 +0200

    Hotfix

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,9 +1,9 @@
 class Bike < ApplicationRecord
     belongs_to :user, foreign_key: "user_id"
     has_one_attached  :photo
-  #  belongs_to :district, foreign_key: "district_id"
+  #  belongs_to :district, foreign_key: "district_id"  >> DO NOT ENABLE THIS LINE IT WILL BREAK THINGS 
     geocoded_by :address
-    after_validation :geocode, if: :will_save_change_to_address?
+    after_validation :geocode,# if: :will_save_change_to_address? HOTFX
     include PgSearch::Model
     pg_search_scope :search_by_title_and_description,
       against: [ :title, :description ],
